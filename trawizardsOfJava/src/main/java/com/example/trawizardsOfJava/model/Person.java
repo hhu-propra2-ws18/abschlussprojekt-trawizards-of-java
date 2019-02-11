83b66347f95de3394bc0c30910eba49042963be7
SherinSchneider <github@s226658945.online.de>
2019-02-11 14:46:38 +0100

    Person hinzugefügt

---
+++ trawizardsOfJava/src/main/java/com/example/trawizardsOfJava/model/Person.java
@@ -0,0 +1,18 @@
+package com.example.trawizardsOfJava.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Person {
+	private String name;
+	@Id
+	private String benutzername;
+	private String email;
+	private boolean admin;
+	//private ArrayList<Gegenstand> gegenstaende;
+	//ProPay
+}
---
+++ trawizardsOfJava/src/main/java/com/example/trawizardsOfJava/web/Controller.java
@@ -0,0 +1,6 @@
+package com.example.trawizardsOfJava.web;
+
+
+public class Controller {
+
+}

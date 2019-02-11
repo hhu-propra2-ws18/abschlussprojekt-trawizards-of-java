cb2a3906095b4fcf2979304d619c00e2621ee894
Marc <mbengsch@me.com>
2019-02-11 12:35:14 +0100

    baseProject

---
+++ trawizardsOfJava/src/main/java/com/example/trawizardsOfJava/TrawizardsOfJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.trawizardsOfJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrawizardsOfJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrawizardsOfJavaApplication.class, args);
+	}
+
+}
+

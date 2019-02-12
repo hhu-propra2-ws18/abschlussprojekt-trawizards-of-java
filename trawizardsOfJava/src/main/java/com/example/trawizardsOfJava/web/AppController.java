8cd6d48a2d0be8f9e4bceac732b04adbf679b8f4
Marc Bengsch <mbengsch@me.com>
2019-02-12 10:58:32 +0100

    UebersichtAndRegMerged

--- trawizardsOfJava/src/main/java/com/example/trawizardsOfJava/web/AppController.java
+++ trawizardsOfJava/src/main/java/com/example/trawizardsOfJava/web/AppController.java
@@ -1,6 +1,8 @@
 package com.example.trawizardsOfJava.web;
 
+import com.example.trawizardsOfJava.data.ArtikelRepository;
 import com.example.trawizardsOfJava.data.BenutzerRepository;
+import com.example.trawizardsOfJava.model.Artikel;
 import com.example.trawizardsOfJava.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,11 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class AppController {
 
 	@Autowired
 	BenutzerRepository benutzerRepository;
+
+	@Autowired
+	ArtikelRepository artikelRepository;
+
+	@GetMapping("/")
+	public String uebersicht(Model model){
+
+		List<Artikel> alleArtikel = artikelRepository.findAll();
+
+		model.addAttribute("artikel", alleArtikel);
+
+		return "uebersichtSeite";
+	}
 
 	@GetMapping("/benutzerverwaltung/{benutzername}")
 	public String benutzerverwaltung(Model model, @PathVariable String benutzername){

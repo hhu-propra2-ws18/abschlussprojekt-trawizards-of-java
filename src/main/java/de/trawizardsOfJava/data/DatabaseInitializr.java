b833439a1da15974db609c9df3635754f495b8fd
Marc Bengsch <mbengsch@me.com>
2019-02-12 14:58:43 +0100

    detailSeiteHinzugefügt

--- src/main/java/de/trawizardsOfJava/data/DatabaseInitializr.java
+++ src/main/java/de/trawizardsOfJava/data/DatabaseInitializr.java
@@ -2,12 +2,15 @@ package de.trawizardsOfJava.data;
 
 
 import de.trawizardsOfJava.model.Artikel;
+import de.trawizardsOfJava.model.Person;
+import de.trawizardsOfJava.model.Verfuegbarkeit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.time.LocalDate;
 
 @Component
 public class DatabaseInitializr implements ServletContextInitializer {
@@ -19,7 +22,7 @@ public class DatabaseInitializr implements ServletContextInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         System.out.println("Populating the database");
-        
+
     }
 
 }

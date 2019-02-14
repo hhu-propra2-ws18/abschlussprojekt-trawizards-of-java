cd8bc51b1bbc3037705167d26180fe07bfeeaabd
Aydin Seven <aysev100@hhu.de>
2019-02-14 13:28:05 +0000

    Trying Mail
    build.gradle dependency, MailController added und application properties angepasst

---
+++ src/main/java/de/trawizardsOfJava/mail/MailController.java
@@ -0,0 +1,43 @@
+package de.trawizardsOfJava.mail;
+
+import de.trawizardsOfJava.data.BenutzerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.mail.internet.MimeMessage;
+
+@Controller
+public class MailController {
+
+    @Autowired
+    JavaMailSender sender;
+
+    @Autowired
+    BenutzerRepository benutzerRepository;
+
+    @GetMapping("/send")
+    @ResponseBody
+    public String home(){
+        try {
+            sendEmail();
+            return "Email gesendet!";
+        }
+        catch(Exception e){
+            return "Email konnte nicht gesendet werden";
+        }
+    }
+
+    private void sendEmail() throws Exception{
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(message);
+
+        messageHelper.setTo(benutzerRepository.findByBenutzername("root").get().getEmail());
+        messageHelper.setText("Hi");
+        messageHelper.setSubject("Wilkommen bei Leih24!");
+        sender.send(message);
+    }
+}

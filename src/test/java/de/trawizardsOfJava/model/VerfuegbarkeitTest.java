8bc892e22ea120eaf83680971817f7437954bfab
Luca Kutzner <lucakutzner@gmail.com>
2019-02-16 17:05:03 +0100

    Tests fixed

--- src/test/java/de/trawizardsOfJava/model/VerfuegbarkeitTest.java
+++ src/test/java/de/trawizardsOfJava/model/VerfuegbarkeitTest.java
@@ -5,13 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class VerfuegbarkeitTest {
-    Verfuegbarkeit verfuegbarkeit = new Verfuegbarkeit();
-
     @Test
-    public void toVerfuegbarkeit() {
+    public void testVerfuegbarkeit() {
         String s = "01/02/2018 - 31/05/2019";
-        verfuegbarkeit.toVerfuegbarkeit(s);
-        assertEquals("2019-05-31", verfuegbarkeit.endDate.toString());
-        assertEquals("2018-02-01", verfuegbarkeit.startDate.toString());
+        Verfuegbarkeit verfuegbarkeit = new Verfuegbarkeit(s);
+        assertEquals("2019-05-31", verfuegbarkeit.getEndDate().toString());
+        assertEquals("2018-02-01", verfuegbarkeit.getStartDate().toString());
     }
 }

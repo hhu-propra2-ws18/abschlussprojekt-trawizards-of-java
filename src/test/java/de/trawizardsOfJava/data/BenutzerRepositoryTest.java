0912a04c2362fc425e87c48b9d950e34902e3d02
SherinSchneider <github@s226658945.online.de>
2019-02-21 09:47:19 +0100

    Meldung für leere Listen hinzugefügt

---
+++ src/test/java/de/trawizardsOfJava/data/BenutzerRepositoryTest.java
@@ -0,0 +1,14 @@
+package de.trawizardsOfJava.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BenutzerRepositoryTest {
+
+	@Test
+	public void scheicherePerson(){
+
+	}
+
+}

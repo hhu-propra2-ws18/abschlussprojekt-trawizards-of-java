6cd1c905804bfa21231bd9cc59abe5f7b8f7288a
Luca Kutzner <lucakutzner@gmail.com>
2019-02-22 15:10:45 +0100

    Repo tests hinzugefügt

---
+++ src/test/java/de/trawizardsOfJava/data/AusleiheRepositoryTest.java
@@ -0,0 +1,60 @@
+package de.trawizardsOfJava.data;
+
+import de.trawizardsOfJava.model.Artikel;
+import de.trawizardsOfJava.model.Ausleihe;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AusleiheRepositoryTest {
+	@Autowired
+	private AusleiheRepository ausleihen;
+	@Autowired
+	private ArtikelRepository artikel;
+
+	@Test
+	public void speicherAusleihe(){
+		Artikel artikel = new Artikel();
+		artikel.setVerleiherBenutzername("foo");
+		artikel.setArtikelName("Bagger");
+		artikel.setBeschreibung("Das ist ein Bagger");
+		artikel.setStandort("Duesseldorf");
+		artikel.setPreis(0);
+		artikel.setKaution(0);
+		Ausleihe ausleihe = new Ausleihe(artikel, null, "bar");
+		this.artikel.save(artikel);
+		this.ausleihen.save(ausleihe);
+
+		ArrayList<Ausleihe> ausleihenListe = this.ausleihen.findByVerleiherName("foo");
+
+		Assertions.assertThat(ausleihenListe.get(0).getAusleihender()).isEqualTo("bar");
+	}
+
+	@Test
+	public void mehrereAusleihen() {
+		Artikel artikel = new Artikel();
+		artikel.setVerleiherBenutzername("foo");
+		artikel.setArtikelName("Bagger");
+		artikel.setBeschreibung("Das ist ein Bagger");
+		artikel.setStandort("Duesseldorf");
+		artikel.setPreis(0);
+		artikel.setKaution(0);
+		Ausleihe ausleihe = new Ausleihe(artikel, null, "bar");
+		this.artikel.save(artikel);
+		this.ausleihen.save(ausleihe);
+		ausleihe = new Ausleihe(artikel, null, "rab");
+		this.ausleihen.save(ausleihe);
+
+		ArrayList<Ausleihe> ausleihenListe = this.ausleihen.findByVerleiherName("foo");
+
+		Assertions.assertThat(ausleihenListe.get(0).getAusleihender()).isEqualTo("bar");
+		Assertions.assertThat(ausleihenListe.get(1).getAusleihender()).isEqualTo("rab");
+	}
+}
---
+++ src/test/java/de/trawizardsOfJava/data/RueckgabeRepositoryTest.java
@@ -0,0 +1,64 @@
+package de.trawizardsOfJava.data;
+
+import de.trawizardsOfJava.model.Artikel;
+import de.trawizardsOfJava.model.Ausleihe;
+import de.trawizardsOfJava.model.Rueckgabe;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class RueckgabeRepositoryTest {
+	@Autowired
+	private RueckgabeRepository rueckgaben;
+	@Autowired
+	private ArtikelRepository artikel;
+
+	@Test
+	public void speicherRueckgabe() {
+		Artikel artikel = new Artikel();
+		artikel.setVerleiherBenutzername("foo");
+		artikel.setArtikelName("Bagger");
+		artikel.setBeschreibung("Das ist ein Bagger");
+		artikel.setStandort("Duesseldorf");
+		artikel.setPreis(0);
+		artikel.setKaution(0);
+		Ausleihe ausleihe = new Ausleihe(artikel, null, "bar");
+		Rueckgabe rueckgabe = new Rueckgabe(ausleihe);
+		this.artikel.save(artikel);
+		this.rueckgaben.save(rueckgabe);
+
+		ArrayList<Rueckgabe> rueckgabenListe = this.rueckgaben.findByVerleiherName("foo");
+
+		Assertions.assertThat(rueckgabenListe.get(0).getAusleihender()).isEqualTo("bar");
+	}
+
+	@Test
+	public void mehrereRueckgaben() {
+		Artikel artikel = new Artikel();
+		artikel.setVerleiherBenutzername("foo");
+		artikel.setArtikelName("Bagger");
+		artikel.setBeschreibung("Das ist ein Bagger");
+		artikel.setStandort("Duesseldorf");
+		artikel.setPreis(0);
+		artikel.setKaution(0);
+		Ausleihe ausleihe = new Ausleihe(artikel, null, "bar");
+		Rueckgabe rueckgabe = new Rueckgabe(ausleihe);
+		this.artikel.save(artikel);
+		this.rueckgaben.save(rueckgabe);
+		ausleihe = new Ausleihe(artikel, null, "rab");
+		rueckgabe = new Rueckgabe(ausleihe);
+		this.rueckgaben.save(rueckgabe);
+
+		ArrayList<Rueckgabe> rueckgabenListe = this.rueckgaben.findByVerleiherName("foo");
+
+		Assertions.assertThat(rueckgabenListe.get(0).getAusleihender()).isEqualTo("bar");
+		Assertions.assertThat(rueckgabenListe.get(1).getAusleihender()).isEqualTo("rab");
+	}
+}

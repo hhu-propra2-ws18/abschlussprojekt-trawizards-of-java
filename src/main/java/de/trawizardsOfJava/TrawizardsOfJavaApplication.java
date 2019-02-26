ef5468128d8934d2fdfa97d367e521c3f134d795
Luca Kutzner <lucakutzner@gmail.com>
2019-02-26 11:54:45 +0100

    Main aufgeräumt

--- src/main/java/de/trawizardsOfJava/TrawizardsOfJavaApplication.java
+++ src/main/java/de/trawizardsOfJava/TrawizardsOfJavaApplication.java
@@ -1,19 +1,13 @@
 package de.trawizardsOfJava;
 
-import de.trawizardsOfJava.web.UploadController;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.File;
 
 @SpringBootApplication
 @Configuration
 public class TrawizardsOfJavaApplication {
 	public static void main(String[] args) {
-	//	new File(UploadController.uploadDirectory).mkdir();
 		SpringApplication.run(TrawizardsOfJavaApplication.class, args);
 	}
 }

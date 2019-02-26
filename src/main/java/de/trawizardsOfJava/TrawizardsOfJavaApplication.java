91fc70a65cd79cd5477631d9bb162714bd8ec56c
Marc Bengsch <mbengsch@me.com>
2019-02-26 13:33:50 +0100

    @Configuration deleted

--- src/main/java/de/trawizardsOfJava/TrawizardsOfJavaApplication.java
+++ src/main/java/de/trawizardsOfJava/TrawizardsOfJavaApplication.java
@@ -2,10 +2,8 @@ package de.trawizardsOfJava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class TrawizardsOfJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrawizardsOfJavaApplication.class, args);

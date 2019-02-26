b7662cff5808971a12be8895242901fd93bb315c
Luca Kutzner <lucakutzner@gmail.com>
2019-02-26 21:09:08 +0100

    Travis läuft wieder
    wie Usain Bolt

--- src/test/java/de/trawizardsOfJava/messenger/web/ChatControllerTest.java
+++ src/test/java/de/trawizardsOfJava/messenger/web/ChatControllerTest.java
@@ -19,7 +19,6 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;

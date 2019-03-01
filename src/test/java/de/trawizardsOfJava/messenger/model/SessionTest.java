95ef17fa0b9d9a129e72b7140366834bb0293f79
Luca Kutzner <lucakutzner@gmail.com>
2019-03-01 11:44:07 +0100

    Unused Imports entfernt

--- src/test/java/de/trawizardsOfJava/messenger/model/SessionTest.java
+++ src/test/java/de/trawizardsOfJava/messenger/model/SessionTest.java
@@ -7,13 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
-
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
